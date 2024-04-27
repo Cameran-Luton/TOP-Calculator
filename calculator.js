7e61cd2c4419e6383ee1afdc9cab6172c39a75fe
Cameran-Luton <cameranluton5@gmail.com>
2024-04-27 14:02:25 +0100

    made the calculator calculate

--- calculator.js
+++ calculator.js
@@ -1,36 +1,39 @@
 let numberOne = null;
 let operation = null;
 let numberTwo = null;
+let numTwoActive = false;
+let activeOperator = null;
 
-function add(numOne, numTwo) {
+function addition(numOne, numTwo) {
   return numOne + numTwo;
 }
 
-function subtract(numOne, numTwo) {
+function subtraction(numOne, numTwo) {
   return numOne - numTwo;
 }
 
-function multiply(numOne, numTwo) {
+function multiplication(numOne, numTwo) {
   return numOne * numTwo;
 }
 
-function divide(numOne, numTwo) {
-  return numOne / numTwo;
+function division(numOne, numTwo) {
+  return (numOne / numTwo).toPrecision(4);
 }
 
 function operate(numOne, numTwo, operator) {
+  console.log(numOne, numTwo);
   switch (operator) {
-    case "add":
-      return add(numOne, numTwo);
+    case "+":
+      return addition(numOne, numTwo);
 
-    case "subtract":
-      return subtract(numOne, numTwo);
+    case "-":
+      return subtraction(numOne, numTwo);
 
-    case "multiply":
-      return multiply(numOne, numTwo);
+    case "*":
+      return multiplication(numOne, numTwo);
 
-    case "divide":
-      return divide(numOne, numTwo);
+    case "/":
+      return division(numOne, numTwo);
 
     default:
       break;
@@ -39,41 +42,130 @@ function operate(numOne, numTwo, operator) {
 
 const display = document.querySelector(".display");
 function updateDisplay(value) {
-  if (display.textContent == "0") {
-    display.textContent = value;
+  if (display.textContent == "0" || display.textContent == "-0") {
+    display.textContent = display.textContent == "-0" ? -value : value;
+    if (!numTwoActive) {
+      numberOne = Number(display.textContent);
+    } else {
+      numberTwo = Number(display.textContent);
+    }
   } else {
     display.textContent += value;
+    if (!numTwoActive) {
+      numberOne = Number(display.textContent);
+    } else {
+      numberTwo = Number(display.textContent);
+    }
   }
 }
 
 function keyChecker(key) {
-  if (!isNaN(key)) {
+  if (!isNaN(key) && display.textContent.length <= 6) {
     updateDisplay(key);
     console.log("number");
   }
   if (key === "AC") {
-    console.log("clear");
+    display.textContent = "0";
+    if (!isNaN(numberOne)) {
+      numberOne = 0;
+      console.log(numberOne);
+    }
+    if (!isNaN(numberTwo)) {
+      numTwoActive = false;
+      numberTwo = 0;
+      console.log(numberTwo);
+    }
   }
+
+  function equals() {
+    if (!isNaN(numberTwo)) {
+      let answer = operate(numberOne, numberTwo, activeOperator);
+      display.textContent = answer.toString().slice(0, 8);
+      numberOne = Number(display.textContent);
+      numberTwo = null;
+      numTwoActive = false;
+    }
+  }
+  function add() {
+    numTwoActive = true;
+    activeOperator = "+";
+    display.textContent = "0";
+  }
+  function subtract() {
+    numTwoActive = true;
+    activeOperator = "-";
+    display.textContent = "0";
+  }
+  function multiply() {
+    numTwoActive = true;
+    activeOperator = "*";
+    display.textContent = "0";
+  }
+  function divide() {
+    numTwoActive = true;
+    activeOperator = "/";
+    display.textContent = "0";
+  }
+
   if (key === "+/-") {
-    console.log("invert");
+    if (display.textContent[0] != "-") {
+      display.textContent = "-" + display.textContent;
+      if (!numTwoActive) {
+        numberOne = Number(display.textContent);
+      } else {
+        numberTwo = Number(display.textContent);
+      }
+    } else if (display.textContent[0] == "-") {
+      display.textContent = display.textContent.slice(1);
+      if (!numTwoActive) {
+        numberOne = Number(display.textContent);
+      } else {
+        numberTwo = Number(display.textContent);
+      }
+    }
   }
   if (key === "%") {
-    console.log("percent");
+    display.textContent = Number(display.textContent) / 100;
+    if (!numTwoActive) {
+      numberOne = Number(display.textContent);
+    } else {
+      numberTwo = Number(display.textContent);
+    }
   }
   if (key === "/") {
-    console.log("divide");
+    if (!numTwoActive) {
+      divide();
+    } else {
+      equals();
+      divide();
+    }
   }
   if (key === "*") {
-    console.log("multiply");
+    if (!numTwoActive) {
+      multiply();
+    } else {
+      equals();
+      multiply();
+    }
   }
   if (key === "-") {
-    console.log("subtract");
+    if (!numTwoActive) {
+      subtract();
+    } else {
+      equals();
+      subtract();
+    }
   }
   if (key === "+") {
-    console.log("add");
+    if (!numTwoActive) {
+      add();
+    } else {
+      equals();
+      add();
+    }
   }
   if (key === "=") {
-    console.log("equals");
+    equals();
   }
   if (key === ".") {
     console.log("decimal");
